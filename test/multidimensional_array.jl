576c6f2ad46b549037093b0edf4e0730e34f0b86
Sven Berger <berger.sven@gmail.com>
2026-06-03 12:37:58 +0200

    add better test

--- test/multidimensional_array.jl
+++ test/multidimensional_array.jl
@@ -51,6 +51,33 @@
         @test yasfmt("[a;b;;]") == "[a; b;;]"
     end
 
+    @testset "wrapped hcat with ncat separators" begin
+        separators = [";;", ";;;", ";;;;"]
+        styles = (DefaultStyle(), YASStyle(), SciMLStyle())
+
+        for sep in separators
+            cases = (
+                # Source form: hcat rows wrapped before the ncat separator.
+                "x = [a\n     b$(sep)\n     c\n     d]",
+                "x = T[a\n      b$(sep)\n      c\n      d]",
+                # Formatted form: JuliaSyntax reparses this as hcat with separator tokens.
+                "x = [a b$(sep)\n     c d]",
+                "x = T[a b$(sep)\n      c d]",
+            )
+
+            for str in cases
+                @test JuliaSyntax.parseall(JuliaSyntax.GreenNode, str) isa
+                      JuliaSyntax.GreenNode
+
+                for style in styles
+                    formatted = format_text(str, style; join_lines_based_on_source = false)
+                    @test JuliaSyntax.parseall(JuliaSyntax.GreenNode, formatted) isa
+                          JuliaSyntax.GreenNode
+                end
+            end
+        end
+    end
+
     @testset "#608" begin
         s1 = """
         hcat([zeros(1); ones(3)], [zeros(2); ones(2)], [zeros(3); ones(1)], [zeros(1); ones(3)], [zeros(2); ones(2)], [zeros(3); ones(1)])
