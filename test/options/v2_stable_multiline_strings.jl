da40f324305de8ef2497a4935463b5a2928acc6d
Penelope Yong <penelopeysm@gmail.com>
2026-06-14 22:26:05 +0100

    Add more tests

--- test/options/v2_stable_multiline_strings.jl
+++ test/options/v2_stable_multiline_strings.jl
@@ -6,26 +6,58 @@ using Test
 
 @testset "v2_stable_multiline_strings" begin
     @testset "length calculation" begin
-        s = """
-        s = \"""
-            1234567890
-        \"""
-        """
+        @testset "a case where it's different" begin
+            s = """
+            s = \"""
+                1234567890
+            \"""
+            """
 
-        # 10 characters past the column before the opening quote
-        fst = format_to_stage(:fst, s)
-        stringn = fst[1][end]
-        @test stringn.typ == JuliaFormatter.StringN
-        @test stringn.len == 10
+            # 10 characters past the column before the opening quote
+            fst = format_to_stage(:fst, s)
+            stringn = fst[1][end]
+            @test stringn.typ == JuliaFormatter.StringN
+            @test stringn.len == 10
 
-        # Opening quote only
-        fst = format_to_stage(:fst, s; v2_stable_multiline_strings=true)
-        stringn = fst[1][end]
-        @test stringn.typ == JuliaFormatter.StringN
-        @test stringn.len == 3
+            # Opening quote only
+            fst = format_to_stage(:fst, s; v2_stable_multiline_strings=true)
+            stringn = fst[1][end]
+            @test stringn.typ == JuliaFormatter.StringN
+            @test stringn.len == 3
+        end
+
+        @testset "a case where it's the same" begin
+            s = """
+            s = \"""1234567890
+                1234567890
+            \"""
+            """
+            for val in (false, true)
+                fst = format_to_stage(:fst, s; v2_stable_multiline_strings=val)
+                stringn = fst[1][end]
+                @test stringn.typ == JuliaFormatter.StringN
+                @test stringn.len == 13
+            end
+        end
     end
 
-    @testset "an example of differing behaviour" begin
+    @testset "example of docs idempotence" begin
+        s = """
+        foooo((\"""
+        12345\""", g),
+            a, b)
+        """
+        output = """
+        foooo((
+            \"""
+        12345\""",
+            g,
+        ), a, b)
+        """
+        test_format(s, output; margin=21, v2_stable_multiline_strings=true)
+    end
+
+    @testset "example of differing nesting behaviour in docs" begin
         s = """
         throw(ArgumentError(\"""
                             ooohhhhhh a very long thing
@@ -34,6 +66,17 @@ using Test
         for style in ALL_STYLES
             test_format(s, s, style; margin=46, v2_stable_multiline_strings=true)
         end
+
+        ordinary_output = """
+        throw(
+            ArgumentError(
+                \"""
+                ooohhhhhh a very long thing
+                \""",
+            ),
+        )
+        """
+        test_format(s, ordinary_output; margin=46, v2_stable_multiline_strings=false)
     end
 end
 
